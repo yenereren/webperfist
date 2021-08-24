5521aee067ad3257ca7a8a723beb1bfcfd226bca
eren.yener <eren.yener@trendyol.com>
2021-08-24 09:54:19 +0300

    events added

--- src/components/SubFooter.js
+++ src/components/SubFooter.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import Social from './Social';
 
 const SubFooter = props => {
-  const data = useStaticQuery(graphql`
-    query SubFooterQuery {
-      configJson {
-        footer {
-          copyright_text
-          copyright_link
-        }
-      }
-    }
-  `);
   return (
     <div className="sub-footer">
       <div className="container">
--- src/pages/index.js
+++ src/pages/index.js
@@ -166,7 +166,6 @@ export const query = graphql`
       edges {
         node {
           title
-          displayName
           description
           date
           instructor {
