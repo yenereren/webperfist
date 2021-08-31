4b6e64d268603580a28f033d61175e0661abe678
eren.yener <eren.yener@trendyol.com>
2021-08-31 09:33:56 +0300

    slack channel link added

--- src/content/index.md
+++ src/content/index.md
@@ -7,7 +7,9 @@ intro_image_hide_on_mobile: true
 ---
 
 
-Web performans üzerine odaklanmış bir topluluktur.Amacımız ulaştığımız kitlelere, web'i olabildiğince hızlı ve doğru bir şekilde ulaştırmaktır.
+Web performans üzerine odaklanmış bir topluluktur.Amacımız ulaştığımız kitlelere, web'i olabildiğince hızlı ve doğru bir şekilde ulaştırmaktır. Etkinliklerimizden haberdar olmak için Slack kanalımıza katılabilir [#webperfist](https://join.slack.com/t/webperfist/shared_invite/zt-uyzvfm1u-F8WSFjDmfeWWpsuUrsX5tQ) ya da aşağıdaki abone olun butonu ile email listemize kaydolabilirsiniz.
+
+
 
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -56,6 +56,9 @@ const Home = props => {
               <img className="logo" height={config.logo.desktop_height} alt={config.logo.alt} src={config.logo.desktop} />
               <div dangerouslySetInnerHTML={{ __html: intro.html }} />
               <div className="social-media-container">
+                <a href={config.socialMedia.slack} target="blank" className="mr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M14.5 3A1.5 1.5 0 0 1 16 4.5v5a1.5 1.5 0 0 1-3 0v-5A1.5 1.5 0 0 1 14.5 3zm-10 10H6v1.5A1.5 1.5 0 1 1 4.5 13zm8.5 5h1.5a1.5 1.5 0 1 1-1.5 1.5V18zm1.5-5h5a1.5 1.5 0 0 1 0 3h-5a1.5 1.5 0 0 1 0-3zm5-5a1.5 1.5 0 0 1 0 3H18V9.5A1.5 1.5 0 0 1 19.5 8zm-15 0h5a1.5 1.5 0 0 1 0 3h-5a1.5 1.5 0 0 1 0-3zm5-5A1.5 1.5 0 0 1 11 4.5V6H9.5a1.5 1.5 0 0 1 0-3zm0 10a1.5 1.5 0 0 1 1.5 1.5v5a1.5 1.5 0 0 1-3 0v-5A1.5 1.5 0 0 1 9.5 13z"/></svg>
+                </a>
                 <a href={config.socialMedia.youtube} target="blank" className="mr-2">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M19.606 6.995c-.076-.298-.292-.523-.539-.592C18.63 6.28 16.5 6 12 6s-6.628.28-7.069.403c-.244.068-.46.293-.537.592C4.285 7.419 4 9.196 4 12s.285 4.58.394 5.006c.076.297.292.522.538.59C5.372 17.72 7.5 18 12 18s6.629-.28 7.069-.403c.244-.068.46-.293.537-.592C19.715 16.581 20 14.8 20 12s-.285-4.58-.394-5.005zm1.937-.497C22 8.28 22 12 22 12s0 3.72-.457 5.502c-.254.985-.997 1.76-1.938 2.022C17.896 20 12 20 12 20s-5.893 0-7.605-.476c-.945-.266-1.687-1.04-1.938-2.022C2 15.72 2 12 2 12s0-3.72.457-5.502c.254-.985.997-1.76 1.938-2.022C6.107 4 12 4 12 4s5.896 0 7.605.476c.945.266 1.687 1.04 1.938 2.022zM10 15.5v-7l6 3.5-6 3.5z"/></svg>
                 </a>
@@ -137,7 +140,8 @@ export const query = graphql`
         socialMedia {
           youtube,
           github,
-          twitter
+          twitter,
+          slack
         },
         logo {
           alt
